e9fe578cb55dc7577c27da9abe004fc0dc17b9be
José Gabriel <46488774+josebarbarine@users.noreply.github.com>
2026-06-03 20:13:08 -0300

    Migrate app from FastAPI to Flask
    Refactor application to use Flask instead of FastAPI, and update the home route.

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-import psycopg
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
-from aluno_dao import AlunoDAO
+from flask import Flask, Response
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return Response("OK AZURE 2.1", status=200)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)  
+    
 
 
-dados_conexao = "host=127.0.0.1 port=5432 dbname=postgres user=postgres password=1910"
 
-app = FastAPI()
-dao = AlunoDAO(dados_conexao)
-print("dao aluno carregado")
 
-class Aluno(BaseModel):
-    id: int
-    nome: str
-    email: str
+    
 
-@app.post("/alunos", status_code=201)
-def criar_aluno(aluno: Aluno):
-    dao.inserir_aluno(aluno.nome, aluno.email)
-    return {"message": "Aluno criado com sucesso!"}
 
-@app.get("/alunos")
-def listar_alunos():
-    resultados = dao.listar_todos()
-    mensagem = ""
-    for aluno in resultados:
-        mensagem = mensagem + f"aluno {aluno.nome}"
-
-    return {"message": mensagem}
 
