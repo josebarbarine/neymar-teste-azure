99fd0ec7280e609769250d6d4e2c72a8dd3dc7ab
José Gabriel <46488774+josebarbarine@users.noreply.github.com>
2026-06-03 21:07:43 -0300

    Refactor Aluno class and API endpoints formatting

--- main.py
+++ main.py
@@ -16,21 +16,20 @@ dao = AlunoDAO(dados_conexao)
 print("dao aluno carregado")
 
 class Aluno(BaseModel):
-    id: int
-    nome: str
-    email: str
+    id: int
+    nome: str
+    email: str
 
 @app.post("/alunos", status_code=201)
 def criar_aluno(aluno: Aluno):
-    dao.inserir_aluno(aluno.nome, aluno.email)
-    return {"message": "Aluno criado com sucesso!"}
+    dao.inserir_aluno(aluno.nome, aluno.email)
+    return {"message": "Aluno criado com sucesso!"}
 
 @app.get("/alunos")
 def listar_alunos():
-    resultados = dao.listar_todos()
-    mensagem = ""
-    for aluno in resultados:
-        mensagem = mensagem + f"aluno {aluno.nome}"
+    resultados = dao.listar_todos()
+    mensagem = ""
+    for aluno in resultados:
+        mensagem = mensagem + f"aluno {aluno.nome}"
 
-    return {"message": mensagem}
-
+    return {"message": mensagem}
